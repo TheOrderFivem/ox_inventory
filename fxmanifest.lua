31af1327cabc6c6de94ce721498d70808dd0d0d0
MrNewb <47620135+MrNewb@users.noreply.github.com>
2025-05-21 15:39:33 -0400

    Migrate

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ game 'gta5'
 name 'ox_inventory'
 author 'Overextended'
 version '2.45.0'
-repository 'https://github.com/communityox/ox_inventory'
+repository 'https://github.com/The-Order-Of-The-Sacred-Framework/ox_inventory'
 description 'Slot-based inventory with item metadata support'
 
 dependencies {
