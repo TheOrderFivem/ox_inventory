79c83ba1d822f8491685357c8755c70e9cbd1521
MrNewb <47620135+MrNewb@users.noreply.github.com>
2025-05-21 15:41:44 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 game 'gta5'
 name 'ox_inventory'
 author 'Overextended'
-version '2.44.2'
+version '2.44.3'
 repository 'https://github.com/The-Order-Of-The-Sacred-Framework/ox_inventory'
 description 'Slot-based inventory with item metadata support'
 
