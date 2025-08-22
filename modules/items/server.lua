9cb832e469d2da25ea25dc07c485440e40b1942e
MrNewb <47620135+MrNewb@users.noreply.github.com>
2025-08-22 15:38:51 -0400

    Revert "Update server.lua"
    This reverts commit 34b6c23b872f7d18a2e1899bbae10dbc381ab7ef.

--- modules/items/server.lua
+++ modules/items/server.lua
@@ -290,10 +290,6 @@ function Items.Metadata(inv, item, metadata, count)
 		if item.hash == `WEAPON_PETROLCAN` or item.hash == `WEAPON_HAZARDCAN` or item.hash == `WEAPON_FERTILIZERCAN` or item.hash == `WEAPON_FIREEXTINGUISHER` then
 			metadata.ammo = metadata.durability
 		end
-
-        if item.rarity then
-			metadata.rarity = item.rarity
-		end
 	else
 		local container = Items.containers[item.name]
 
@@ -318,10 +314,6 @@ function Items.Metadata(inv, item, metadata, count)
 
 		if not metadata.durability then
 			metadata = setItemDurability(ItemList[item.name], metadata)
-		end
-
-        if item.rarity then
-			metadata.rarity = item.rarity
 		end
 	end
 
