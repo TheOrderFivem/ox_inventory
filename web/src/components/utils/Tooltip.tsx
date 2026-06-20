42deeefdb9965f1287f54013c0074075e8321612
aysihuniks <askinceyhun78@gmail.com>
2026-06-20 20:19:07 +0300

    feat(nui): sync item tooltip metadata dynamically from inventory store

--- web/src/components/utils/Tooltip.tsx
+++ web/src/components/utils/Tooltip.tsx
@@ -1,10 +1,28 @@
 import { flip, FloatingPortal, offset, shift, useFloating, useTransitionStyles } from '@floating-ui/react';
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { useAppSelector } from '../../store';
 import SlotTooltip from '../inventory/SlotTooltip';
+import { isSlotWithItem } from '../../helpers';
 
 const Tooltip: React.FC = () => {
   const hoverData = useAppSelector((state) => state.tooltip);
+  const leftInventory = useAppSelector((state) => state.inventory.leftInventory);
+  const rightInventory = useAppSelector((state) => state.inventory.rightInventory);
+
+  const currentItem = useMemo(() => {
+    if (!hoverData.open || !hoverData.item || !hoverData.inventoryType) return null;
+    const isPlayer = hoverData.inventoryType === 'player';
+    const inventory = isPlayer ? leftInventory : rightInventory;
+    const slotIndex = hoverData.item.slot - 1;
+    const latestItem = inventory?.items?.[slotIndex];
+    if (latestItem && latestItem.name === hoverData.item.name && isSlotWithItem(latestItem)) {
+      return latestItem;
+    }
+    if (latestItem && latestItem.name !== hoverData.item.name) {
+      return null;
+    }
+    return hoverData.item;
+  }, [hoverData.open, hoverData.item, hoverData.inventoryType, leftInventory, rightInventory]);
 
   const { refs, context, floatingStyles } = useFloating({
     middleware: [flip(), shift(), offset({ mainAxis: 10, crossAxis: 10 })],
@@ -43,12 +61,12 @@ const Tooltip: React.FC = () => {
 
   return (
     <>
-      {isMounted && hoverData.item && hoverData.inventoryType && (
+      {isMounted && currentItem && hoverData.inventoryType && (
         <FloatingPortal>
           <SlotTooltip
             ref={refs.setFloating}
             style={{ ...floatingStyles, ...styles }}
-            item={hoverData.item!}
+            item={currentItem}
             inventoryType={hoverData.inventoryType!}
           />
         </FloatingPortal>
